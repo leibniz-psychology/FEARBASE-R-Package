2d7b95d55c8d6a9d8bc122a8914908edf62a8c5c
Fritz Becker <fritzthec4t@gmail.com>
2026-03-04 14:24:02 +0100

    give the visualization defaults their own file.

---
+++ R/viz-defaults.R
@@ -0,0 +1,30 @@
+fearbase_palette <- c(
+  "#ffece2",
+  "#ff8800",
+  "#0032A0"
+)
+
+generate_palette <- function(n_colors) {
+  if (n_colors == 1) {
+    fearbase_palette[3]
+  } else if (n_colors == 2) {
+    fearbase_palette[c(3, 2)]
+  } else if (n_colors == 3) {
+    fearbase_palette[c(3, 2, 1)]
+  } else {
+    grDevices::colorRampPalette(
+      fearbase_palette,
+      space = "Lab",
+      interpolate = "spline"
+    )(n_colors)
+  }
+}
+
+set_theme(theme_classic())
+
+update_theme(
+  palette.colour.discrete = generate_palette,
+  palette.fill.discrete = generate_palette,
+  palette.fill.continuous = fearbase_palette,
+  palette.color.continuous = fearbase_palette
+)
