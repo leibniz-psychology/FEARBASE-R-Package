7571102e85822c549251974ec33a91aa4a71de87
Fritz Becker <fritzthec4t@gmail.com>
2026-03-04 13:52:42 +0100

    include baseline detection window

--- R/peakDetectionWindows.R
+++ R/peakDetectionWindows.R
@@ -11,25 +11,42 @@ peakDetectionWindows <- function() {
   graph <- metadata |>
     select(
       condition_id,
+      scr_baseline_window_start,
+      scr_baseline_window_end,
       scr_peak_detection_window_min,
       scr_peak_detection_window_max
     ) |>
-    drop_na() |>
+    drop_na(scr_peak_detection_window_max) |>
     distinct() |>
-    mutate(
-      diff = scr_peak_detection_window_max - scr_peak_detection_window_min
-    ) |>
-    arrange(desc(diff), desc(scr_peak_detection_window_max)) |>
+    arrange(desc(scr_peak_detection_window_max)) |>
     mutate(condition_id = factor(condition_id, levels = condition_id)) |>
-    pivot_longer(cols = -c(condition_id, diff), names_to = "window") |>
-    ggplot(aes(x = condition_id, y = value, fill = window, group = window)) +
-    geom_bar(stat = "identity", show.legend = FALSE) +
+    pivot_longer(
+      cols = -c(condition_id),
+      names_to = c("measure", "window", "timepoint"),
+      names_pattern = "(scr)_(.*)_window_(.*)"
+    ) |>
+    mutate(
+      timepoint = forcats::fct_recode(timepoint, start = "min", end = "max")
+    ) |>
+    pivot_wider(
+      names_from = timepoint,
+      values_from = value
+    ) |>
+    ggplot(aes(x = condition_id, color = window, group = window)) +
+    geom_segment(
+      aes(
+        y = start,
+        yend = end
+      ),
+      linewidth = 7
+    ) +
     labs(
-      title = "SCR Peak Detection Windows by Study",
+      title = "SCR Detection Windows by Study",
       x = "Study",
       y = "Time (s)",
       fill = "Window"
-    )
+    ) +
+    coord_flip()
 
   return(graph)
 }
