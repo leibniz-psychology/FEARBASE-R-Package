b70f4051c7e61b83466b59cd1c88eefe182ca8f2
Fritz Becker <fritzthec4t@gmail.com>
2026-03-10 17:16:18 +0100

    change bar graph to pie chart

--- R/stimModality.R
+++ R/stimModality.R
@@ -13,15 +13,29 @@ stimModality <- function(type = "us") {
   ) {
     graph <- md |>
       select(condition_id, study_id, us_type, cs_type) |>
-      ggplot(aes(x = us_type)) +
-      geom_bar()
+      group_by(us_type) |>
+      summarise(n = n()) |>
+      arrange(n) |>
+      mutate(us_type = factor(us_type, levels = us_type)) |>
+      ggplot(aes(x = "", fill = us_type, y = n)) +
+      geom_bar(stat = "identity", width = 1) +
+      coord_polar("y", start = 0) +
+      theme_void(paper = "white") +
+      geom_text(aes(label = n), position = position_stack(vjust = 0.5))
   } else if (
     tolower(type) %in% c("cs", "conditioned stimulus", "conditioned stimuli")
   ) {
     graph <- md |>
       select(condition_id, study_id, us_type, cs_type) |>
-      ggplot(aes(x = cs_type)) +
-      geom_bar()
+      group_by(cs_type) |>
+      summarise(n = n()) |>
+      arrange(n) |>
+      mutate(cs_type = factor(cs_type, levels = cs_type)) |>
+      ggplot(aes(x = "", fill = cs_type, y = n)) +
+      geom_bar(stat = "identity", width = 1) +
+      coord_polar("y", start = 0) +
+      theme_void(paper = "white") +
+      geom_text(aes(label = n), position = position_stack(vjust = 0.5))
   } else {
     stop("unknown stimulus type")
   }
