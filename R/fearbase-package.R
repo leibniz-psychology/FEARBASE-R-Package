f8f66db8836cb528f525448950b11bd23d9450da
Fritz Becker <fritzthec4t@gmail.com>
2026-04-13 09:49:35 +0200

    import dplyr fully rather than individual functions

--- R/fearbase-package.R
+++ R/fearbase-package.R
@@ -2,24 +2,6 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom dplyr arrange
-#' @importFrom dplyr as_tibble
-#' @importFrom dplyr bind_cols
-#' @importFrom dplyr bind_rows
-#' @importFrom dplyr case_when
-#' @importFrom dplyr distinct
-#' @importFrom dplyr filter
-#' @importFrom dplyr group_by
-#' @importFrom dplyr left_join
-#' @importFrom dplyr mutate
-#' @importFrom dplyr n
-#' @importFrom dplyr pull
-#' @importFrom dplyr rename
-#' @importFrom dplyr select
-#' @importFrom dplyr setdiff
-#' @importFrom dplyr summarise
-#' @importFrom dplyr summarize
-#' @importFrom dplyr ungroup
 #' @importFrom readr read_csv
 #' @importFrom tidyr drop_na
 #' @importFrom tidyr pivot_longer
@@ -29,5 +11,6 @@
 
 #' @import ggplot2
 #' @import patchwork
+#' @import dplyr
 
 NULL
